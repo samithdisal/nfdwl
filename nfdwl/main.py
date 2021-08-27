324128d35fd193afbda8d1c3f222f3d173027a44
Samith Sandanayake <samith@sansolue.com>
2021-08-27 07:53:11 +0530

    Handle batches

--- nfdwl/main.py
+++ nfdwl/main.py
@@ -16,11 +16,17 @@ __normalize_map = {
     ord("“"): "\"",
     ord("”"): "\"",
     ord("’"): "'",
+    ord("⅓"): "1/3",
 }
 
 
+def strip_non_ascii(input: str) -> str:
+    return ''.join([n for n in input if ord(n) < 127])
+
+
 def normalise_str(inp: bytes) -> str:
-    return str(inp.decode("utf-8").translate(__normalize_map).encode("ascii", "ignore"), encoding="ascii")
+    n1 = inp.decode("utf-8").translate(__normalize_map)
+    return strip_non_ascii(n1)
 
 
 def get_chapter_urls(url: str):
@@ -47,9 +53,13 @@ def add_chapter(title: str, content: str, book: mkepub.Book):
         i.decompose()
     last_div = chapter.find_all("div")[-1]
     last_div.decompose()
-    book.add_page(title, chapter)
-    print(f"Adding chapter {title}")
-    time.sleep(6)
+    try:
+        book.add_page(title, chapter)
+        print(f"Adding chapter {title}")
+        time.sleep(6)
+    except Exception as e:
+        book.add_page(title, f"********************\n Skipping {title}\n*******************\n\n")
+        print(f"Skipping chapter {title}: {e}")
 
 
 def chunks(lst, n):
@@ -61,6 +71,9 @@ def chunks(lst, n):
 @click.command()
 @click.argument("title")
 @click.argument("url")
+@click.argument("chunk_size", type=int, default=100)
+@click.argument("start_idx", type=int, default=0)
+@click.argument("end_idx", type=int, default=0)
 def main(title: str, url: str, chunk_size: int = 100, start_idx: int = 0, end_idx: int = 0) -> int:
     chapter_urls = get_chapter_urls(url)
     if start_idx and end_idx:
@@ -85,14 +98,14 @@ def main(title: str, url: str, chunk_size: int = 100, start_idx: int = 0, end_id
             print("start index should be positive")
             return 1
 
-    c_urls = chunks(chapter_urls, 100)
+    c_urls = chunks(chapter_urls, chunk_size)
 
     for c in c_urls:
-        book = mkepub.Book(title=f"{title} Chapter {int(c[0])+1} to {int(c[1])}")
+        book = mkepub.Book(title=f"{title} Chapter {start_idx+int(c[0])+1} to {start_idx+int(c[1])}")
         for chapter_title, chapter_url in c[2]:
-            add_chapter(f"{chapter_title}", get_chapter(chapter_url), book)
+            add_chapter(f"{strip_non_ascii(chapter_title)}", get_chapter(chapter_url), book)
             time.sleep(1)
-        filename = f"{title.replace(' ', '_')}_{int(c[0])+1}_{c[1]}.epub"
+        filename = f"{title.replace(' ', '_')}_{start_idx+int(c[0])+1}_{start_idx+c[1]}.epub"
         if os.path.exists(filename):
             os.remove(filename)
         book.save(filename)
